634154fd9b835f26b1f4464f9bb48305ede9dcb7
ShinSooJin <sujin91.shin@gmail.com>
2016-07-29 15:13:37 +0900

    몽고디비까지

--- app.js
+++ app.js
@@ -1,9 +1,68 @@
-var express = require('express');
+var express = require('express'); //include, import express
 var path= require('path');
+var app = express(); //express를 app객체에 넣어 사용하기 쉽게
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB); //DB연결
+var db = mongoose.connection; //mongoose디비를 db객체에
 
-var app = express();
+db.once("open", function() { //연결되면
+  console.log("DB connected!");
+});
+db.on("error", function(err) { //에러나면
+  console.log("DB ERROR :", err);
+});
+//스키마
+var dataScheme = mongoose.Schema({
+  name: String,
+  count: Number
+}); //object를 인자로한 스키마
+var Data = mongoose.model('data', dataScheme); //모델을 담는 변수는 첫글자가 대문자.
+Data.findOne({name:"myData"}, function(err, data) {
+  if(err) return console.log("Data Error:", err);
+  if(!data) {
+    Data.create({name:"myData", count:0}, function (err, data) {
+      if(err) return console.log("Data Error:", err);
+      console.log("Counter initialized :", data);
+    });
+  }
+});
 
+app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+//var data={count:0};
+
+app.get('/', function (req,res) {
+  Data.findOne({name:"myData"}, function(err, data) {
+    if(err) return console.log("Data Error:", err);
+    data.count++;
+    data.save(function (err) {
+      if(err) return console.log("Data Error:", err);
+      res.render('my_first_ejs', data);
+    });
+  });
+});
+
+
+app.get('/reset', function (req,res) {
+  data.count = 0;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/count', function (req,res) {
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/:num', function (req,res) {
+  data.count = req.params.num;
+  res.render('my_first_ejs', data);
+});
+
+
+app.get('/', function (req,res){
+  res.render('my_first_ejs');
+});
 
 app.listen(3000, function() {
   console.log('server On!!');
